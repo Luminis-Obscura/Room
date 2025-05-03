0e4aff936005716b67e2d1036d5552e36fb1309a
Yuhao Chen <yuhao.chen2000@icloud.com>
2025-05-03 14:15:41 -0700

    Lay the main scene out

---
+++ Assets/Scripts/CycleAnimator.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class CycleAnimator : MonoBehaviour
+{
+    GameObject[] frames;
+    public int currentFrame = 0;
+    public int frameDelay = 1;
+    
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
