f99f1376bd86e759c6f6ae4b75b3fac87399d4a7
Yuhao Chen <yuhao.chen2000@icloud.com>
2025-05-06 08:19:14 -0700

    Implement sleep

---
+++ Assets/Scripts/SleepController.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using UnityEngine;
+
+public class SleepController : MonoBehaviour
+{
+    private Animator animator;
+    private bool isSleeping = false;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+
+        if (animator == null)
+        {
+            Debug.LogError("Animator component not found on " + gameObject.name);
+            return;
+        }
+    }
+
+
+    public void Sleep()
+    {
+        animator.Play("Sleep");
+    }
+}
